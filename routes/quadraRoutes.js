ca96e16dfcaf44388e02baf81ffdc7faa9c3da55
Gabriel Gomes Attie <gabrielgattie@gmail.com>
2025-05-19 23:01:07 -0300

    ainda preciso fazer o delete funcionar certinho

--- routes/quadraRoutes.js
+++ routes/quadraRoutes.js
@@ -27,8 +27,9 @@ router.get('/quadra/:id', async (req, res) => {
     try {
         const quadraId = parseInt(req.params.id, 10);
         const quadra = await Quadra.findByPk(quadraId);
+        const user = req.user; // Obtém o usuário logado
         if (quadra) {
-            res.render('quadra', { quadra });
+            res.render('quadra', { quadra, user }); // Passa a quadra e o usuário para a view
         } else {
             res.status(404).send('Quadra não encontrada');
         }
@@ -40,7 +41,8 @@ router.get('/quadra/:id', async (req, res) => {
 
 // Rota para exibir o formulário de adição de quadra
 router.get('/add', (req, res) => {
-    res.render('addQuadra');
+    const user = req.user; // Obtém o usuário logado
+    res.render('addQuadra', { user }); // Passa o usuário para a view
 });
 
 // Rota para adicionar uma nova quadra
@@ -48,7 +50,9 @@ router.post('/add', async (req, res) => {
     try {
         const { nome, localizacao, descricao, preco, imagem, telefone, funcionamento_inicio, funcionamento_fim } = req.body;
         const funcionamento = `${funcionamento_inicio} até ${funcionamento_fim}`;
-        await Quadra.create({ nome, localizacao, descricao, preco, imagem, telefone, funcionamento });
+        const donos_id = req.session.userId; // Obtém o ID do usuário logado
+        console.log('ID do dono:', donos_id); // Verifica se o ID do dono está sendo obtido corretamente
+        await Quadra.create({ nome, localizacao, descricao, preco, imagem, telefone, funcionamento, donos_id });
         res.redirect('/');
     } catch (error) {
         console.error('Erro ao adicionar quadra:', error);
@@ -56,4 +60,24 @@ router.post('/add', async (req, res) => {
     }
 });
 
+// Rota para deletar uma quadra
+router.post('/quadra/:id/delete', async (req, res) => {
+    try {
+        const quadraId = parseInt(req.params.id, 10);
+
+        // Busca a quadra pelo ID
+        const quadra = await Quadra.findByPk(quadraId);
+
+        if (!quadra) {
+            return res.status(404).send('Quadra não encontrada');
+        }
+
+        // Deleta a quadra
+        await quadra.destroy();
+        res.redirect('/');
+    } catch (error) {
+        console.error('Erro ao deletar quadra:', error);
+        res.status(500).send('Erro ao deletar quadra');
+    }
+});
 module.exports = router;
